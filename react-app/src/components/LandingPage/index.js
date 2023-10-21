4c6a53cb0fbdcfeee95335cc799e905a0ceaa2f0
tienhoang <xuantien93@yahoo.com>
2023-10-20 22:35:26 -0400

    adjusting start order

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -4,11 +4,24 @@ import './LandingPage.css'
 
 const LandingPage = () => {
 
-
-
-
     return (
-        <div>Hello World</div>
+        <div className="landing-page" style={{ backgroundImage: 'url(https://i.imgur.com/VyZicmD.jpg)', height: '100vh' }}>
+            <div className="landing-page-content">
+                <div className="orange-line-order">
+                    <div className="red-line-order">
+                        <div className="green-line-order">
+                            <div className="start-order">
+                                <h4>Don't forget to try our delicious Strawberry Cheesecake Supah Shake</h4>
+                                <button className="landing-page-button">Start your order</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
     )
 }
 
