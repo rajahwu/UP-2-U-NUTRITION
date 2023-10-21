c27844fd7abb47204daa753d43550176435fb1a7
tienhoang <xuantien93@yahoo.com>
2023-10-21 19:37:19 -0400

    our story

--- react-app/src/components/OurStory/index.js
+++ react-app/src/components/OurStory/index.js
@@ -6,7 +6,22 @@ const OurStory = () => {
 
 
     return (
-        <div>Our Story</div>
+        <div className="ourstory-page">
+            <div className="ourstory-page-bgimg"></div>
+            <div className="ourstory-page-content">
+                <div id="ourstory-title">OUR STORY</div>
+                <img id="owner-img" src="https://i.imgur.com/mfTz11d.png"></img>
+                <div className="ourstory-body">Ex cillum deserunt irure deserunt qui magna nostrud id cillum proident fugiat cupidatat duis. Excepteur pariatur reprehenderit do commodo do eu proident voluptate consectetur. Lorem incididunt occaecat quis minim. Cupidatat adipisicing minim ut aliqua esse. Lorem magna eu cupidatat quis duis fugiat dolore do id ut.
+
+                    Laborum eu culpa velit enim minim eiusmod dolore officia fugiat. Est laborum laborum labore laboris nostrud et mollit veniam. Magna adipisicing voluptate occaecat et dolore qui consequat. Occaecat voluptate do incididunt laboris. Ipsum labore minim ullamco sint laboris commodo voluptate tempor sunt aliqua. Ipsum commodo id anim eu officia cupidatat commodo tempor consequat magna. Cillum exercitation ullamco tempor enim do enim ut est non. Id cillum do occaecat id ad qui esse culpa et in nulla quis voluptate veniam. Enim aliquip nisi velit commodo est veniam sit elit consequat. Consectetur quis labore sint non pariatur commodo voluptate officia adipisicing non. Laborum consequat occaecat irure eiusmod officia sit exercitation cillum aliquip deserunt nulla consequat aute dolore.
+
+                    Ad occaecat qui enim proident elit dolor reprehenderit irure id adipisicing labore pariatur quis Lorem. Reprehenderit sunt id adipisicing id ut ea. Fugiat qui laborum consequat nisi elit est ut anim nostrud aliqua elit laboris id.
+
+                    Occaecat veniam quis aute mollit. Sit nisi sint in exercitation anim. Exercitation sit anim velit amet dolore duis in labore deserunt quis dolore sunt. Eiusmod irure consequat laborum cupidatat cupidatat. Dolor consequat minim qui ex nostrud incididunt ea. Pariatur occaecat ut occaecat ut labore voluptate aliqua mollit do duis.
+
+                    Do in ad mollit deserunt ad laboris consequat sit ut esse irure deserunt quis duis. Quis minim culpa tempor excepteur consectetur cupidatat. Fugiat id occaecat nisi et. Qui enim dolore in dolore proident qui ipsum mollit esse quis sint culpa cupidatat incididunt. Ex est enim nostrud id nulla tempor laborum labore voluptate quis exercitation magna ad. Incididunt occaecat anim deserunt ipsum qui exercitation eiusmod enim id adipisicing ullamco.</div>
+            </div>
+        </div>
     )
 }
 
