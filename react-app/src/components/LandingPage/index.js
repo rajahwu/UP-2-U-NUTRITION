cb64a79fa844fd10d3316a1c69281b9e57e9c73a
tienhoang <xuantien93@yahoo.com>
2023-10-20 23:37:58 -0400

    start button

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import './LandingPage.css'
-
+import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const LandingPage = () => {
+
+    const history = useHistory()
+
 
     return (
         <div className="landing-page" style={{ backgroundImage: 'url(https://i.imgur.com/VyZicmD.jpg)', height: '100vh' }}>
@@ -12,14 +16,12 @@ const LandingPage = () => {
                         <div className="green-line-order">
                             <div className="start-order">
                                 <h2>Don't forget to try our delicious Strawberry Cheesecake Supah Shake</h2>
-                                <button className="landing-page-button">START YOUR ORDER</button>
+                                <button onClick={() => history.push("/menu")} className="landing-page-button">START YOUR ORDER</button>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
     )
 }
