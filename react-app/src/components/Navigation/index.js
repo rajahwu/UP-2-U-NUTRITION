483ea2cacabccc741639a18c23364507f9612666
tienhoang <xuantien93@yahoo.com>
2023-10-24 12:48:59 -0400

    finsihed nav

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,14 +8,11 @@ import './Navigation.css';
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
 	const [menuImgSrc, setMenuImgSrc] = useState("https://i.imgur.com/6HBCQjQ.png");
+	const [ourstoryImgSrc, setOurstoryImgSrc] = useState("https://i.imgur.com/Jq9BowW.png")
+	const [eventsImgSrc, setEventsImgSrc] = useState("https://i.imgur.com/cPARxw9.png")
+	const [yourstoryImgSrc, setYourstoryImgSrc] = useState("https://i.imgur.com/5hyTKlI.png")
+	const [cartImgSrc, setCartImgSrc] = useState('https://i.imgur.com/Aar6YWm.png')
 
-	const handleMenuHover = () => {
-		setMenuImgSrc("https://i.imgur.com/bmwn5DQ.png");
-	};
-
-	const handleMenuLeave = () => {
-		setMenuImgSrc("https://i.imgur.com/6HBCQjQ.png");
-	};
 
 	return (
 		<div className="nav-bar">
@@ -27,25 +24,37 @@ function Navigation({ isLoaded }) {
 				</div>
 				<div className="logo2">
 					<NavLink exact to="/our-story">
-						<img id="sub-icon" src="https://i.imgur.com/Jq9BowW.png" className="ourstory-1" alt="Our Story" />
+						<img id="sub-icon" src={ourstoryImgSrc}
+							onMouseEnter={() => setOurstoryImgSrc("https://i.imgur.com/bmwn5DQ.png")}
+							onMouseLeave={() => setOurstoryImgSrc("https://i.imgur.com/Jq9BowW.png")}
+							className="ourstory-1" alt="Our Story" />
 					</NavLink>
 					<NavLink exact to="/menu">
 						<img
 							id="sub-icon"
 							src={menuImgSrc}
 							alt="Menu"
-							onMouseEnter={handleMenuHover}
-							onMouseLeave={handleMenuLeave}
+							onMouseEnter={() => setMenuImgSrc("https://i.imgur.com/7f7cBSk.png")}
+							onMouseLeave={() => setMenuImgSrc("https://i.imgur.com/6HBCQjQ.png")}
 						/>
 					</NavLink>
 					<NavLink exact to="/events">
-						<img id="sub-icon" src="https://i.imgur.com/cPARxw9.png" alt="Events" />
+						<img id="sub-icon" src={eventsImgSrc}
+							onMouseEnter={() => setEventsImgSrc("https://i.imgur.com/0roGKGR.png")}
+							onMouseLeave={() => setEventsImgSrc("https://i.imgur.com/cPARxw9.png")}
+							alt="Events" />
 					</NavLink>
 					<NavLink exact to="/your-story">
-						<img id="sub-icon" src="https://i.imgur.com/5hyTKlI.png" alt="Your Story" />
+						<img id="sub-icon" src={yourstoryImgSrc}
+							onMouseEnter={() => setYourstoryImgSrc("https://i.imgur.com/NKP818A.png")}
+							onMouseLeave={() => setYourstoryImgSrc("https://i.imgur.com/5hyTKlI.png")}
+							alt="Your Story" />
 					</NavLink>
 					<NavLink exact to="/cart">
-						<img id="sub-icon" src="https://i.imgur.com/Aar6YWm.png" alt="Cart" />
+						<img id="sub-icon" src={cartImgSrc}
+							onMouseEnter={() => setCartImgSrc("https://i.imgur.com/OFbZmNV.png")}
+							onMouseLeave={() => setCartImgSrc("https://i.imgur.com/Aar6YWm.png")}
+							alt="Cart" />
 					</NavLink>
 				</div>
 			</div>
