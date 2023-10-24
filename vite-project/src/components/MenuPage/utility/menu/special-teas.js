104ff27517a53374bf964f98c932c9f699191737
haaannn123 <haaannn97@gmail.com>
2023-10-24 18:46:55 -0400

    vite-project

---
+++ vite-project/src/components/MenuPage/utility/menu/special-teas.js
@@ -0,0 +1,177 @@
+export const specialTeas = [
+    {
+        'id': 1,
+        'name': 'Mint Tea',
+        'category': 'supah shakes',
+        'image': '/images/teas/tea-temp.jpeg',
+        'ingredients': [
+            'Cream Cheese',
+            'Graham Cracker Crumbs',
+            'Powdered Sugar',
+            'Strawberry Ice Cream',
+            'Strawberry Milk',
+            'Fresh Strawberries',
+            'White Cake'],
+        'nutrition_table': [
+            {
+                'nutrient': 'Total Carbohydrate',
+                'weight': '10mg',
+                'percentage': 10
+            },
+            {
+                'nutrient': 'Dietary fiber',
+                'weight': '0.5mg',
+                'percentage': 2
+            },
+            {
+                'nutrient': 'Sugar',
+                'weight': '27g'
+            },
+            {
+                'nutrient': 'Protein',
+                'percentage': 14
+            },
+
+        ]
+    },
+    {
+        'id': 2,
+        'name': 'Green Tea',
+        'category': 'supah shakes',
+        'image': '/images/teas/tea-temp.jpeg',
+        'ingredients': [
+            'Cream Cheese',
+            'Graham Cracker Crumbs',
+            'Powdered Sugar',
+            'Strawberry Ice Cream',
+            'Strawberry Milk',
+            'Fresh Strawberries',
+            'White Cake'],
+        'nutrition_table': [
+            {
+                'nutrient': 'Total Carbohydrate',
+                'weight': '10mg',
+                'percentage': 10
+            },
+            {
+                'nutrient': 'Dietary fiber',
+                'weight': '0.5mg',
+                'percentage': 2
+            },
+            {
+                'nutrient': 'Sugar',
+                'weight': '27g'
+            },
+            {
+                'nutrient': 'Protein',
+                'percentage': 14
+            },
+
+        ]
+    },
+    {
+        'id': 3,
+        'name': 'Energy Tea',
+        'category': 'supah shakes',
+        'image': '/images/teas/tea-temp.jpeg',
+        'ingredients': [
+            'Cream Cheese',
+            'Graham Cracker Crumbs',
+            'Powdered Sugar',
+            'Strawberry Ice Cream',
+            'Strawberry Milk',
+            'Fresh Strawberries',
+            'White Cake'],
+        'nutrition_table': [
+            {
+                'nutrient': 'Total Carbohydrate',
+                'weight': '10mg',
+                'percentage': 10
+            },
+            {
+                'nutrient': 'Dietary fiber',
+                'weight': '0.5mg',
+                'percentage': 2
+            },
+            {
+                'nutrient': 'Sugar',
+                'weight': '27g'
+            },
+            {
+                'nutrient': 'Protein',
+                'percentage': 14
+            },
+
+        ]
+    },
+    {
+        'id': 4,
+        'name': 'Black Tea',
+        'category': 'supah shakes',
+        'image': '/images/teas/tea-temp.jpeg',
+        'ingredients': [
+            'Cream Cheese',
+            'Graham Cracker Crumbs',
+            'Powdered Sugar',
+            'Strawberry Ice Cream',
+            'Strawberry Milk',
+            'Fresh Strawberries',
+            'White Cake'],
+        'nutrition_table': [
+            {
+                'nutrient': 'Total Carbohydrate',
+                'weight': '10mg',
+                'percentage': 10
+            },
+            {
+                'nutrient': 'Dietary fiber',
+                'weight': '0.5mg',
+                'percentage': 2
+            },
+            {
+                'nutrient': 'Sugar',
+                'weight': '27g'
+            },
+            {
+                'nutrient': 'Protein',
+                'percentage': 14
+            },
+
+        ]
+    },
+    {
+        'id': 5,
+        'name': 'Teas Me',
+        'category': 'supah shakes',
+        'image': '/images/teas/tea-temp.jpeg',
+        'ingredients': [
+            'Cream Cheese',
+            'Graham Cracker Crumbs',
+            'Powdered Sugar',
+            'Strawberry Ice Cream',
+            'Strawberry Milk',
+            'Fresh Strawberries',
+            'White Cake'],
+        'nutrition_table': [
+            {
+                'nutrient': 'Total Carbohydrate',
+                'weight': '10mg',
+                'percentage': 10
+            },
+            {
+                'nutrient': 'Dietary fiber',
+                'weight': '0.5mg',
+                'percentage': 2
+            },
+            {
+                'nutrient': 'Sugar',
+                'weight': '27g'
+            },
+            {
+                'nutrient': 'Protein',
+                'percentage': 14
+            },
+
+        ]
+    },
+];
