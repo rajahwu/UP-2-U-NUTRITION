3b4b13afd42a71347bbe065a5936f5f4e2f37e12
Peter Joh <johpeter@gmail.com>
2023-10-27 18:57:26 -0600

    heyo it was a proxy issue

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -37,12 +37,16 @@ def login():
     # Get the csrf_token from the request cookie and put it into the
     # form manually to validate_on_submit can be used
     # form['csrf_token'].data = request.cookies['csrf_token']
-    # if form.validate_on_submit():
+    if 'csrf_token' in request.cookies:
+        form['csrf_token'].data = request.cookies['csrf_token']
+    else:
+        return {'errors': ['CSRF token missing']}, 400
+    if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-    user = User.query.filter(User.email == form.data['email']).first()
-    login_user(user)
-    return user.to_dict()
-    # return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+        user = User.query.filter(User.email == form.data['email']).first()
+        login_user(user)
+        return user.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @auth_routes.route('/logout')
