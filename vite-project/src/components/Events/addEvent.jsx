455056a85842d30631ce7a0ef2662ba21ca43709
David Sexton Jr <davesextonjr@gmail.com>
2023-10-30 19:32:23 -0400

    rough in add event form

--- vite-project/src/components/Events/addEvent.jsx
+++ vite-project/src/components/Events/addEvent.jsx
@@ -13,13 +13,18 @@ export function AddEvent() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if(allDay){
+      setStartTime("0:00")
+      setEndTime("23:59")
+    }
+
     const event = {
       title,
       details,
       startDate,
       endDate,
-      startTime,
-      endTime,
+      "startTime": `${startDate}T${startTime}:00`,
+      "endTime": `${endDate}T${endTime}:00`,
       color,
     };
     console.log(event);
@@ -62,33 +67,33 @@ export function AddEvent() {
             className="bg-gray-100 rounded text-center h-10 txt-lg"
             id="end-date"
             type="date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            required
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
           />
         </div>
 
         {!allDay && (
-            <div className="flex place-content-between">
-              <div>
-                <label htmlFor="startTime">Start Time:</label>
-                <input
-                  id="startTime"
-                  type="time"
-                  value={startTime}
-                  onChange={(e) => setStartTime(e.target.value)}
-                />
-              </div>
-              <div>
-                <label htmlFor="endTime">End Time:</label>
-                <input
-                  id="endTime"
-                  type="time"
-                  value={endTime}
-                  onChange={(e) => setEndTime(e.target.value)}
-                />
-              </div>
+          <div className="flex place-content-between">
+            <div>
+              <label htmlFor="startTime">Start Time:</label>
+              <input
+                id="startTime"
+                type="time"
+                value={startTime}
+                required
+                onChange={(e) => setStartTime(e.target.value)}
+              />
             </div>
+            <div>
+              <label htmlFor="endTime">End Time:</label>
+              <input
+                id="endTime"
+                type="time"
+                value={endTime}
+                onChange={(e) => setEndTime(e.target.value)}
+              />
+            </div>
+          </div>
         )}
         <div className="flex space-x-3">
           <label htmlFor="all-day">All Day:</label>
@@ -102,15 +107,35 @@ export function AddEvent() {
         </div>
         <div className="flex flex-col space-y-3">
           <label htmlFor="color">Choose Color:</label>
-          <input
-            className="bg-gray-100 rounded text-center h-10 txt-lg"
+          <select
+            className={`bg-[${color}] rounded text-center h-10 txt-lg`}
             id="color"
-            type="select"
             value={color}
             onChange={(e) => setColor(e.target.value)}
             required
+          >
+            <option
+            value="" disabled hidden>
+              Choose a Color
+            </option>
+            <option value="#d98c00" >Orange</option>
+            <option value="#d52540" >Red</option>
+            <option value="#5eac00" >Green</option>
+            <option value="#0093d3">Blue</option>
+          </select>
+        </div>
+        <div className="flex flex-col space-y-3">
+          <label htmlFor="event-details">Event Details:</label>
+          <textarea
+            className="bg-gray-100 rounded  h-100 txt-lg resize-none p-2"
+            id="event-details"
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+            placeholder="Enter event details here"
+            required
           />
         </div>
+
         <button>Submit Event</button>
       </form>
     </div>
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-orange': '#d98c00',
+        'theme-red': "#d52540",
+        'theme-green': "#5eac00",
+        'theme-blue': "#0093d3"
+      }
+    },
   },
   variants:{
     opacity:['disabled', 'first']
