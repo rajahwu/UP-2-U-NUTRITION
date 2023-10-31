8c5bc77a4e4d32d68538f9967351dcc59714b7e0
Daniel Blanco <db@d-blanco.com>
2023-10-31 08:59:42 -0400

    WIP email - phone submit button logic

--- vite-project/src/components/OurStory/utility/contactUs.jsx
+++ vite-project/src/components/OurStory/utility/contactUs.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 
+// import ReactWhatsapp from "react-whatsapp";
+
 import './contactUs.css'
 
 export const ContactUsModal = () => {
@@ -10,6 +12,21 @@ export const ContactUsModal = () => {
     const [isValid, setIsValid] = useState(true);
 
     const [state, handleSubmit] = useForm('mknlylwj')
+
+    // const [numberEmptyError, setNumberEmptyError] = useState(false);
+    // const [messageEmptyError, setMessageEmptyError] = useState(false);
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const phoneRegex = /^[0-9]{10}$/;
+
+    // const onChange = (e) => {
+    //     e.preventDefault();
+    //     setFormData({
+    //         ...formData,
+    //         [e.target.name]: e.target.value,
+    //     });
+    // };
+
     if (state.succeeded) {
         window.alert('Message sent!')
     }
@@ -21,9 +38,6 @@ export const ContactUsModal = () => {
     }
 
     const validateEmailOrPhone = (inputValue) => {
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        const phoneRegex = /^[0-9]{10}$/;
-
         if (emailRegex.test(inputValue) || phoneRegex.test(inputValue)) {
             setIsValid(true);
         } else {
@@ -31,8 +45,27 @@ export const ContactUsModal = () => {
         }
     }
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(emailOrPhone)
+        if (phoneRegex.test(emailOrPhone)) {
+
+            let number = emailOrPhone.replace(/[^\w\s]/gi, "").replace(/ /g, "");
+
+            let url = `https://web.whatsapp.com/send?phone=1${number}`;
+
+            url += `&text=${encodeURI(description)}&app_absent=0`;
+
+            console.log(url)
+
+            window.open(url)
+        }
+    };
+
     return (
         <form className="contact-us-form flex flex-col gap-2 h-full contact-us-width" onSubmit={handleSubmit}>
+            {/* <ReactWhatsapp number='1-323-449-8300' message='test!!!' /> */}
             <input
                 className="bg-white h-9"
                 type="text"
@@ -76,7 +109,9 @@ export const ContactUsModal = () => {
                 field="message"
                 errors={state.errors}
             />
-            <button type="submit" disabled={state.submitting}>Send</button>
+            <button type="submit" disabled={state.submitting}
+            // onClick={onSubmit}
+            >Send</button>
         </form>
     )
 }
