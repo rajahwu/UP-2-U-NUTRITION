1d214d41dfc3c5bb632d49d20b7f45f1f808f8be
David Sexton Jr <davesextonjr@gmail.com>
2023-11-05 14:37:11 -0500

    clean up code

--- vite-project/src/components/Events/addEvent.jsx
+++ vite-project/src/components/Events/addEvent.jsx
@@ -12,13 +12,15 @@ export function AddEvent() {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-  const user = useSelector(state => state.session.user)
+  const user = useSelector((state) => state.session.user);
 
   //get today's date formatted "YYYY-mm-dd"
-  const todayString = new Date().toISOString().slice(0,10)
+  const todayString = new Date().toISOString().slice(0, 10);
 
-
+  //useLocation collects state passed from useNavigate
   const event = location.state;
+
+  //set variables to pass when creating state based on whether data is being passed to the form or not (is it new event or edit event)
   const tempTitle = event && event.title ? event.title : "";
   const tempDetails = event && event.details ? event.details : "";
   const tempStartDate =
@@ -31,6 +33,7 @@ export function AddEvent() {
     event && event.end_time ? event.end_time.slice(-12, -7) : "";
   const tempColor = event && event.color ? event.color : "";
 
+  //set controled inputs for the form
   const [title, setTitle] = useState(tempTitle);
   const [details, setDetails] = useState(tempDetails);
   const [startDate, setStartDate] = useState(tempStartDate);
@@ -38,12 +41,11 @@ export function AddEvent() {
   const [startTime, setStartTime] = useState(tempStartTime);
   const [endTime, setEndTime] = useState(tempEndTime);
   const [color, setColor] = useState(tempColor);
-  // const [errors, setErrors] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // setErrors([])
 
+    //build the correct data shape
     const eventToSend = {
       title,
       details,
@@ -53,15 +55,14 @@ export function AddEvent() {
       endTime: `${endDate} ${endTime}:00`,
       color,
     };
+
+    //check if we should edit an existing event or create a new one
     if (event) {
       eventToSend.id = event.id;
       console.log(eventToSend);
-      const data = await dispatch(editEventThunk(eventToSend));
-      console.log(data);
-
+      dispatch(editEventThunk(eventToSend));
       navigate("/events");
     } else {
-      console.log(eventToSend);
       const data = dispatch(createEventThunk(eventToSend));
       if (data.errors) {
         console.log(data.errors);
@@ -77,6 +78,7 @@ export function AddEvent() {
       navigate("/events");
     }
   };
+
   const handleDelete = (e) => {
     e.preventDefault();
     if (window.confirm("Are you sure you want to delete this event?")) {
@@ -86,114 +88,112 @@ export function AddEvent() {
   };
 
   return (
-    <>{user && user.admin ?
+    <>
+      {user && user.admin ? (
+        <div className="w-full max-w-lg m-auto">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col m-auto space-y-6 mb-4 "
+          >
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="event-title">Event Title:</label>
+              <input
+                className="bg-gray-100 rounded text-center h-10 txt-lg"
+                id="event-title"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title must be between 3 and 100 charaters"
+                minLength="3"
+                maxLength="100"
+                required
+              />
+            </div>
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="start-date">Start Date:</label>
+              <input
+                className="bg-gray-100 rounded text-center h-10 txt-lg"
+                id="start-date"
+                type="date"
+                min={todayString}
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+                required
+              />
+            </div>
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="end-date">End Date:</label>
+              <input
+                className="bg-gray-100 rounded text-center h-10 txt-lg"
+                id="end-date"
+                type="date"
+                min={startDate || todayString}
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
 
-      <div className="w-full max-w-lg m-auto">
-      <form
-      onSubmit={handleSubmit}
-      className="bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col m-auto space-y-6 mb-4 "
-      >
-      <div className="flex flex-col space-y-3">
-      <label htmlFor="event-title">Event Title:</label>
-      <input
-      className="bg-gray-100 rounded text-center h-10 txt-lg"
-      id="event-title"
-      type="text"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      placeholder="Title must be between 3 and 100 charaters"
-      minLength="3"
-      maxLength="100"
-      required
-      />
-      </div>
-      <div className="flex flex-col space-y-3">
-      <label htmlFor="start-date">Start Date:</label>
-      <input
-      className="bg-gray-100 rounded text-center h-10 txt-lg"
-      id="start-date"
-      type="date"
-      min={todayString}
-      value={startDate}
-      onChange={(e) => setStartDate(e.target.value)}
-      required
-      />
-      </div>
-      <div className="flex flex-col space-y-3">
-      <label htmlFor="end-date">End Date:</label>
-      <input
-      className="bg-gray-100 rounded text-center h-10 txt-lg"
-      id="end-date"
-      type="date"
-      min={startDate || todayString}
-      value={endDate}
-      onChange={(e) => setEndDate(e.target.value)}
-      />
-      </div>
+            <div className="flex place-content-between">
+              <div>
+                <label htmlFor="startTime">Start Time:</label>
+                <input
+                  id="startTime"
+                  type="time"
+                  value={startTime}
+                  required
+                  onChange={(e) => setStartTime(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="endTime">End Time:</label>
+                <input
+                  id="endTime"
+                  type="time"
+                  value={endTime}
+                  onChange={(e) => setEndTime(e.target.value)}
+                />
+              </div>
+            </div>
 
-      <div className="flex place-content-between">
-      <div>
-      <label htmlFor="startTime">Start Time:</label>
-      <input
-      id="startTime"
-      type="time"
-      value={startTime}
-      required
-      onChange={(e) => setStartTime(e.target.value)}
-      />
-      </div>
-      <div>
-      <label htmlFor="endTime">End Time:</label>
-      <input
-      id="endTime"
-      type="time"
-      value={endTime}
-      onChange={(e) => setEndTime(e.target.value)}
-      />
-      </div>
-      </div>
-
-      <div className="flex flex-col space-y-3">
-      <label htmlFor="color">Choose Color:</label>
-      <select
-      className={`bg-[${color}] rounded text-center h-10 txt-lg`}
-      id="color"
-      value={color}
-      onChange={(e) => setColor(e.target.value)}
-      required
-      >
-      <option value="" disabled hidden>
-      Choose a Color
-      </option>
-      <option value="#d98c00">Orange</option>
-      <option value="#d52540">Red</option>
-      <option value="#5eac00">Green</option>
-      <option value="#0093d3">Blue</option>
-      </select>
-      </div>
-      <div className="flex flex-col space-y-3">
-      <label htmlFor="event-details">Event Details:</label>
-      <textarea
-      className="bg-gray-100 rounded  h-100 txt-lg resize-none p-2"
-      id="event-details"
-      value={details}
-      onChange={(e) => setDetails(e.target.value)}
-      placeholder="Enter event details here"
-      required
-      />
-      </div>
-      <button>Submit Event</button>
-      <button onClick={handleCancel}>Cancel</button>
-      {event && <button onClick={handleDelete}>Delete Event</button>}
-      </form>
-      </div>
-  :
-   <div>
-     <div onClick={navigate("events")}>Click here to return to events</div>
-  </div>
-
-
-          }
-      </>
-      );
-    }
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="color">Choose Color:</label>
+              <select
+                className={`bg-[${color}] rounded text-center h-10 txt-lg`}
+                id="color"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                required
+              >
+                <option value="" disabled hidden>
+                  Choose a Color
+                </option>
+                <option value="#d98c00">Orange</option>
+                <option value="#d52540">Red</option>
+                <option value="#5eac00">Green</option>
+                <option value="#0093d3">Blue</option>
+              </select>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="event-details">Event Details:</label>
+              <textarea
+                className="bg-gray-100 rounded  h-100 txt-lg resize-none p-2"
+                id="event-details"
+                value={details}
+                onChange={(e) => setDetails(e.target.value)}
+                placeholder="Enter event details here"
+                required
+              />
+            </div>
+            <button>Submit Event</button>
+            <button onClick={handleCancel}>Cancel</button>
+            {event && <button onClick={handleDelete}>Delete Event</button>}
+          </form>
+        </div>
+      ) : (
+        <div>
+          <div onClick={navigate("events")}>Click here to return to events</div>
+        </div>
+      )}
+    </>
+  );
+}
--- vite-project/src/components/Events/requestEvents.jsx
+++ vite-project/src/components/Events/requestEvents.jsx
@@ -1,64 +1,100 @@
+
 import { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 export const RequestEventModal = () => {
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [emailOrPhone, setEmailOrPhone] = useState("");
-  const [description, setDescription] = useState("");
+    const [name, setName] = useState('');
+    const [eventTitle, setEventTitle] = useState('')
+    const [emailOrPhone, setEmailOrPhone] = useState('');
+    const [description, setDescription] = useState('');
+    const [isValid, setIsValid] = useState(true);
+    const [state, handleSubmit] = useForm('mknlylwj');
+    const [startTime, setStartTime] = useState("");
+    const [endTime, setEndTime] = useState("");
 
-  const [isValid, setIsValid] = useState(true);
+    const todayString = new Date().toISOString().slice(0,16);
 
 
-  const todayString = new Date().toISOString().slice(0,16);
-  console.log(todayString)
 
-  console.log(startTime)
-
-  const handleEmailOrPhone = (e) => {
-    const value = e.target.value;
-    setEmailOrPhone(value);
-    validateEmailOrPhone(value);
-  };
-
-  const validateEmailOrPhone = (inputValue) => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     const phoneRegex = /^[0-9]{10}$/;
 
-    if (emailRegex.test(inputValue) || phoneRegex.test(inputValue)) {
-      setIsValid(true);
-    } else {
-      setIsValid(false);
-    }
-  };
+    // const onChange = (e) => {
+    //     e.preventDefault();
+    //     setFormData({
+    //         ...formData,
+    //         [e.target.name]: e.target.value,
+    //     });
+    // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (isValid) {
-      console.log("Input is Valid: ", emailOrPhone);
-    } else {
-      console.log("Input is not Valid: ", emailOrPhone);
+    if (state.succeeded) {
+        window.alert('Message sent!')
     }
-  };
+
+    const handleEmailOrPhone = (e) => {
+        const value = e.target.value;
+        setEmailOrPhone(value);
+        validateEmailOrPhone(value);
+    }
+
+    const validateEmailOrPhone = (inputValue) => {
+        if (emailRegex.test(inputValue) || phoneRegex.test(inputValue)) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const messageDetails = `RequesterName: ${name}, RequestedEventTitle: ${eventTitle}, RequestedEventStartTime: ${startTime}, RequestedEventEndTime: ${endTime}, RequestedEventDescription${description}`
+
+        console.log(emailOrPhone)
+        if (phoneRegex.test(emailOrPhone)) {
+
+            let number = emailOrPhone.replace(/[^\w\s]/gi, "").replace(/ /g, "");
+
+            let url = `https://web.whatsapp.com/send?phone=1${number}`;
+
+            url += `&text=${encodeURI(messageDetails)}&app_absent=0`;
+
+            console.log(url)
+
+            window.open(url)
+        }
+    };
 
   return (
-    <div className="w-full max-w-3xl m-auto">
-      <div className="event-bar event-green-bar w-full"></div>
+    <div className=" w-full m-auto px-8">
+      <div className="event-bar event-green-bar h -mx-10"></div>
       <div className="headers form-titles p-3">REQUEST EVENT</div>
       <div className="divider"></div>
       <div className="request-event-form-btn w-full flex flex-col p-8">
         <form
-          className="pt-8 flex flex-col space-y-10 "
+          className="pt-8 flex flex-col space-y-10 w-5/6 "
           onSubmit={handleSubmit}
         >
-          <div className="form-start-and-end flex flex-col space-y-10 ">
-            <div className="flex space-x-3 place-content-center">
+          <div className="flex flex-col space-y-3" >
+      <label htmlFor="event-title">Event Title:</label>
+      <input
+      className="bg-gray-100 rounded text-center txt-lg h-10"
+      id="event-title"
+      type="text"
+      value={eventTitle}
+      onChange={(e) => setEventTitle(e.target.value)}
+      placeholder="Title must be between 3 and 100 charaters"
+      minLength="3"
+      maxLength="100"
+      required
+      />
+      </div>
+          {/* <div className="form-start-and-end flex flex-col space-y-10 "> */}
+            <div className="flex flex-col space-y-3">
               <label
-                htmlFor="item-start
-                            "
-                className="p-3"
+                htmlFor="item-start"
               >
-                Start:
+                Requested Start:
               </label>
               <input
                 className="bg-gray-100 rounded text-center h-10 txt-lg p-4"
@@ -70,9 +106,9 @@ export const RequestEventModal = () => {
                 required
               />
             </div>
-            <div className="flex space-x-3 place-content-center">
-              <label htmlFor="item-end" className="p-3">
-                End:
+            <div className="flex flex-col space-y-3">
+              <label htmlFor="item-end">
+                Requested End:
               </label>
               <input
                 className="bg-gray-100 rounded text-center h-10 txt-lg p-4"
@@ -84,24 +120,25 @@ export const RequestEventModal = () => {
                 required
               />
             </div>
-          </div>
+          {/* </div> */}
           {isValid ? null : <div style={{ color: "red" }}>Invalid Input</div>}
 
-          <div className="flex space-y-4 w-5/6 m-auto flex-col place-content-center">
+          <div className="flex flex-col space-y-3 place-content-center">
             <label htmlFor="item-description" className="pt-3 pl-1">
               Description:
             </label>
             <textarea
-              className="bg-gray-100 rounded  h-100 txt-lg resize-none p-3"
+              className="bg-gray-100 rounded  h-[30vh] txt-lg resize-none p-3"
               id="item-description"
               type="text"
               value={description}
+              maxLength="300"
               onChange={(e) => setDescription(e.target.value)}
               required
             />
           </div>
-          <div className="flex space-x-3 pt-7 place-content-center">
-            <label htmlFor="item-email-phone" className="p-3">
+          <div className="flex flex-col space-y-3">
+            <label htmlFor="item-email-phone">
               Email/Phone:
             </label>
             <input
@@ -113,6 +150,20 @@ export const RequestEventModal = () => {
               required
             />
           </div>
+          <div className="flex flex-col space-y-3">
+      <label htmlFor="event-title">Name:</label>
+      <input
+      className="bg-gray-100 rounded text-center h-10 txt-lg"
+      id="event-title"
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      placeholder="Please Enter Your Name"
+      minLength="3"
+      maxLength="50"
+      required
+      />
+      </div>
           <div className="request-event-btn-container">
             <button className="green-btn request-event-btn" type="submit">
               Submit
