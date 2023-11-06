060c5fad4ed7dd0ea431cea12a18d313199e8388
haaannn123 <haaannn97@gmail.com>
2023-11-05 22:47:49 -0500

    removed dependencies

--- vite-project/src/components/LandingPage/index.jsx
+++ vite-project/src/components/LandingPage/index.jsx
@@ -6,20 +6,20 @@ import { useSelector } from 'react-redux';
 const LandingPage = () => {
     const navigate = useNavigate();
     const user = useSelector(state => state.session.user)
-    // const dailySpecials = [
-    //     'Give Back Weekend: Come Saturday & Sunday, and with the minimum purchase of $7 get an entry for our raffle. If you bring a new friend that orders, get an extra entry.',
-    //     'Brew-tiful Monday: $1 off in any of our coffees.',
-    //     'Turn-Up Tuesday: Get a special gift with your purchase.',
-    //     'Win it Wednesday: $2 off in our waffles & waffle combos.',
-    //     'ThirsTea Thursday: Buy One Get One ½ off in any of our teas (SpecialTeas & InsaniTeas).',
-    //     'Freaky Friday: Double punch day for loyalty card holders.',
-    //     'Give Back Weekend: Come Saturday & Sunday, and with the minimum purchase of $7 get an entry for our raffle. If you bring a new friend that orders, get an extra entry.',
-    // ]
+    const dailySpecials = [
+        'Give Back Weekend: Come Saturday & Sunday, and with the minimum purchase of $7 get an entry for our raffle. If you bring a new friend that orders, get an extra entry.',
+        'Brew-tiful Monday: $1 off in any of our coffees.',
+        'Turn-Up Tuesday: Get a special gift with your purchase.',
+        'Win it Wednesday: $2 off in our waffles & waffle combos.',
+        'ThirsTea Thursday: Buy One Get One ½ off in any of our teas (SpecialTeas & InsaniTeas).',
+        'Freaky Friday: Double punch day for loyalty card holders.',
+        'Give Back Weekend: Come Saturday & Sunday, and with the minimum purchase of $7 get an entry for our raffle. If you bring a new friend that orders, get an extra entry.',
+    ]
 
     const currentDate = new Date().getDay();
-    // const displaySpecials = (specials) => {
-    //     return specials[currentDate]
-    // }
+    const displaySpecials = (specials) => {
+        return specials[currentDate]
+    }
 
     return (
         <>
@@ -32,9 +32,10 @@ const LandingPage = () => {
                                     <h1 className="font-bold text-3xl mt-1">YOUR HOME AWAY FROM HOME</h1>
                                     {user ? (
                                         <button onClick={() => navigate("/menu")} className="landing-page-button">START ORDER</button>
-                                    ): (
-                                        <button onClick={() => navigate("/login")} className="landing-page-button">START HERE</button>
-                                    )}
+                                        ): (
+                                            <button onClick={() => navigate("/login")} className="landing-page-button">START HERE</button>
+                                            )}
+                                    <h4 className="text-align p-1">{displaySpecials(dailySpecials)}</h4>
                                 </div>
                             </div>
                         </div>
